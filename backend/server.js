1a949db5ea76e6fcf93458e1392531a170827bfd
Raj <rj1828660@gmail.com>
2022-11-05 15:27:26 +0530

    portChanged

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,15 @@ const bodyParser=require("body-parser");
 const cors=require("cors");
 
 const app=express();
-const PORT= process.env.PORT || 5000;
-
+const PORT= process.env.PORT || 3000;
+//Middlewares
+app.use(express.json())//handle json data
+app.use(express.urlencoded({extend: false}))//handle data through url
+app.use(bodyParser.json());//helps to parse data from front-end to backend
+//Routes
+app.get("/",()=>(req,res)=>{
+  res.send("Home Page");
+})
 //Connect mongoDb
 mongoose
   .connect(process.env.MONGO_URI)
